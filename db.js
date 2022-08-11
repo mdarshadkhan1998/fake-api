8609a0c6be03920448a1a2c50840d5551c8de34d
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-08-11 20:28:35 +0530

    .

---
+++ db.js
@@ -0,0 +1,31 @@
+const items = require("./data/db");
+const data = require("./data/db.json");
+module.exports = {
+  data,
+  items
+}
+
+
+// const items = require("./data/items");
+
+// module.exports = () => {
+
+//   var tags = items.map((i) => i.tags);
+//   tags = [].concat.apply([], tags);
+//   tags = [...new Set(tags)];
+
+ 
+
+//   var brands = items.map((i) => i.manufacturer);
+//   brands = [...new Set(brands)];
+
+//   var itemTypes = items.map((i) => i.itemType);
+//   itemTypes = [...new Set(itemTypes)];
+
+//   return {
+//     items,
+//     tags,
+//     brands,
+//     itemTypes,
+//   };
+// };
